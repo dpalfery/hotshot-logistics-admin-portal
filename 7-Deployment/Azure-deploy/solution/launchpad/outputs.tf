d87a8f1943d87c16f0a8810c21b056346a03edb8
David Palfery <dave@palfery.com>
2025-11-08 18:18:40 -0500

    removing old terraform

--- 7-Deployment/Azure-deploy/solution/launchpad/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = azurerm_resource_group.launchpad.name
-}
-
-output "storage_account_name" {
-  description = "The name of the storage account"
-  value       = azurerm_storage_account.remote_state.name
-} 
